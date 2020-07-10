a06cf9911d3a6b94944157cb9c9047925a714c8b
David Foster <dlfosterii@yahoo.com>
2020-07-10 15:44:50 -0400

    twilio pulling from emergency contact DB

--- routes/sms.js
+++ routes/sms.js
@@ -5,9 +5,19 @@ const twilio = require('../module/twilio')
 
 
 router.post('/alert', function (req, res, next) {
-    twilio.sendMultiSms(['+14077098738', '+14702658975'], 'here goes nothing' )
-    .then(message => {
-        res.json(message);
+    db.User.findByPk(req.params.id, {
+        include: [{
+            model: db.EmergencyContact
+        }]
+    }).then((user) => {
+        let emergencyNumbers = [];
+        db.EmergencyContact.forEach(contact => {
+            emergencyNumbers.push(contact.phoneNumber)
+        })
+        twilio.sendMultiSms(emergencyNumbers, 'This is from the app')
+            .then(message => {
+                res.json(message);
+            })
     })
 });
 
